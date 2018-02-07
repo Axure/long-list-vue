bda780dc7dc5033100fd8abb3cf13e42ef21a601
郑虎 <zhenghu@bytedance.com>
2018-02-07 20:09:18 +0800

    Optimize with chunked list.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <template v-for="(v, i) in slices">
+      <HelloWorld msg="Welcome to Your Vue.js App" :key="i" :data="slice(i)" :handler="setValue" :slice="i"/>
+    </template>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld
+    },
+    data(){
+      return {
+        elements: new Array(80000).fill(0).map(() => Math.random() * 1000 | 1)
+      }
+    },
+    computed: {
+      slices(){
+        return new Array(Math.ceil(this.elements.length / 1000)).fill(0).map((e, i) => i);
+      },
+    },
+    methods: {
+      slice(i){
+        return this.elements.slice(i * 1000, (i + 1) * 1000)
+      },
+      setValue(i, value){
+        console.log(this, 'we are dealing with');
+        this.$set(this.elements, i, value);
+        // this.elements = this.elements.slice();
+        console.log(this.elements)
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,9 @@
     <div>{{msg}}</div>
     <button v-on:click="append">append manually</button>
     <ul>
-      <li v-for="(hehe, i) in displayList" v-bind:key="hehe">
+      <li v-for="(hehe, i) in displayList" v-bind:key="i">
         {{hehe}}
-        <input  v-model="haha[i]"/>
+        <input v-on:keyup="setValue(i + slice * 1000, 1)"/>
       </li>
     </ul>
   </div>
@@ -16,44 +16,42 @@
     name: 'HelloWorld',
     props: {
       msg: String,
+      slice: Number,
+      data: Array,
+      handler: Function
     },
     data: () => ({
-      haha: new Array(8000).fill(0).map((e, i) => i),
       displayList: []
     }),
+    watch: {
+      data: function() {
+        this.displayList = [];
+        this.append();
+      }
+    },
     methods: {
       append(){
         console.log('We are appending');
         const length = this.displayList.length;
-        this.displayList = this.displayList.concat(this.haha.slice(length, length + 1000));
-        if (this.displayList.length < this.haha.length){
+        this.displayList = this.displayList.concat(this.data.slice(length, length + 1000));
+        if (this.displayList.length < this.data.length){
           requestAnimationFrame(() =>{
             this.append();
           })
         }
+      },
+      setValue(i, value) {
+        console.log('We are setting the value');
+        this.handler(i, value);
       }
     },
-    computed: {}
+    computed: {},
+    mounted() {
+      this.append();
+    }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
 </style>
